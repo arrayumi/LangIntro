e4b44f190d1cbb59c2a04b5e067aa50c3f09dfa6
Anna <thoria@yandex.ru>
2022-09-20 14:58:54 +0300

    add task 04

---
+++ Task04/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Введите третье целое число: ");
+int c = Convert.ToInt32 (Console.ReadLine());
+
+int numMax = a;
+if (b > numMax)
+{
+numMax = b;
+}
+else 
+{
+numMax = a;
+}
+if (c > numMax)
+{
+numMax = c;
+}
+else
+{
+    numMax = a;
+}
+Console.Write(numMax);
