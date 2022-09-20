98ef04f599e92f3672dee754f7d3efa8856ff914
Anna <thoria@yandex.ru>
2022-09-20 12:06:32 +0300

    add program task 01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое целое число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int square = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine($"Квадрат числа {number} = {square}?");
+if ( square == number * number )
+{
+    Console.Write("да");
+}
+else
+{
+     Console.Write("нет");
+}
