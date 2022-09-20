d15aeb7d57ddf284134e6496c03cceb3f5833b34
Anna <thoria@yandex.ru>
2022-09-20 15:38:37 +0300

    add Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число больше нуля: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int i = 1;
+
+if (num >=1 )
+{
+while (i <= num)
+{
+ if (i%2 == 0)
+ {
+    Console.Write($"{i} ");
+    i++;
+ }
+ else
+ {
+    i++;
+ }
+}
+}
+else
+{
+   Console.Write("Ошибка. Введите целое число больше нуля");
+}
