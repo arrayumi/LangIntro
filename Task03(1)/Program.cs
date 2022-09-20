d7f0e3c60175fc64628341ed96988bdc11e0b93f
Anna <thoria@yandex.ru>
2022-09-20 12:39:05 +0300

    Add Task 03(1)

---
+++ Task03(1)/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите целое число от 1 до 7: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+switch (number)
+{
+case 1:
+    Console.Write("понедельник");
+    break;
+case 2:
+    Console.Write("вторник");
+    break;
+case 3:
+    Console.Write("среда");
+    break;
+case 4:
+    Console.Write("четверг");
+    break;
+case 5:
+    Console.Write("пятница");
+    break;
+case 6:
+    Console.Write("суббота");
+    break;
+case 7:
+    Console.Write("воскресенье");
+    break;
+default:
+Console.Write("Ошибка. Перезапустите программу и введите целое число от 1 до 7");
+    break;
+}
