c92673f75d447221de3faf9aee1549fda67a26da
Anna <thoria@yandex.ru>
2022-09-24 02:18:29 +0300

    Task10 homework

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,34 +1,23 @@
-﻿// 11. Напишите программу, которая выводит случайное
-// трёхзначное число и удаляет вторую цифру этого
-// числа.
-// 456 -> 46
-// 782 -> 72
-// 918 -> 98
+﻿// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное трехзначное число - {number}.");
 
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное трехзначное число - {number}.");
+Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-// !!!!! Невнимательно прочитала условия задачи и нашла вторую цифру числа. Пусть будет. 
-//
-//
-//int SecondDigit(int num)
-// {
-//     int firstDigit = num / 100;
-//     int thirdDigit = num % 10;
-//     int secondDigit = (num - (firstDigit * 100) - thirdDigit) / 10;
-//     return secondDigit;
-// }
-
-// int second = SecondDigit(number);
-// Console.WriteLine($"Вторая цифра числа {number} - {second}.");
-
-int NoSecondDigit(int num)
+int SecondDigit(int num)
 {
     int firstDigit = num / 100;
     int thirdDigit = num % 10;
-    int result = firstDigit * 10 + thirdDigit;
-    return result;
+    int secondDigit = (num - (firstDigit * 100) - thirdDigit) / 10;
+    return secondDigit;
 }
 
-int res = NoSecondDigit(number);
-Console.WriteLine(res);
+if (number < 1000 && number > 99)
+{
+    int second = SecondDigit(number);
+    Console.WriteLine($"Вторая цифра числа {number} - {second}.");
+}
+else
+{
+    Console.WriteLine("Введите целое трехзначное число");
+}
