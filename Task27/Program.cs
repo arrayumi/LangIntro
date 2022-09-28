b6ce30b8bc85013827d39f736c690c51de6baaac
Anna <thoria@yandex.ru>
2022-09-28 22:03:06 +0300

    Task27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = default;
+    while (num > 9) // >=9 - почему программа работает корректно?
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    sum = sum + num;
+    return sum;
+}
+
+Console.WriteLine(SumDigits(number));
