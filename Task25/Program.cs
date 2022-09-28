516b88d7dedeb0887a19a70de5fa5f20df271f0e
Anna <thoria@yandex.ru>
2022-09-28 21:21:35 +0300

    ternary operator added

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,13 +3,13 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write ("Введите число A: ");
+Console.Write("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.Write ("Введите число B: ");
+Console.Write("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-int Expo (int numA, int numB)
+int Expo(int numA, int numB)
 {
     int result = 1;
     for (int counter = 1; counter <= numB; counter++)
@@ -19,4 +19,5 @@ int Expo (int numA, int numB)
     return result;
 }
 
-Console.WriteLine (Expo (numberA,numberB));
+int res = numberB == 0 ? res = 1 : res = Expo(numberA, numberB);
+Console.WriteLine(res);
