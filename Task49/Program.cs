7d57fbf440af4644c1aa26d085088c65e92acf66
Anna <thoria@yandex.ru>
2022-10-09 12:27:53 +0300

    Task49 formating

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -23,23 +23,23 @@ void PrintMatrix(int[,] matrix)
         Console.Write("[");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
-            else Console.Write($"{matrix[i, j], 4}");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
         }
         Console.WriteLine("]");
     }
 }
 
-int [,] SquareEvenIndex (int [,] matrix)
+int[,] SquareEvenIndex(int[,] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        if (i%2==0 && j%2==0) matrix [i, j] *= matrix [i,j];
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i, j];
+        }
     }
-}
-return matrix;
+    return matrix;
 }
 
 int[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
