1f20f1781620cd2967725aaca34a01607fdb91bd
Anna <thoria@yandex.ru>
2022-10-15 09:35:27 +0300

    Task55 added

---
+++ Task55/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+// 1 2 3 4  4 4 3 1  
+// 3 2 1 0  3 3 2 2 
+// 4 3 2 2  1 2 1 3
+// 4 3 1 2  2 2 0 4
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}| ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] MatrixFlipOver(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = matrix[j, i];
+        }
+    }
+    return newMatrix;
+}
+
+void MatrixFlipOver2(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[j, i];
+            matrix[j, i] = matrix[i, j];
+            matrix[i, j] = temp;
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+
+if (array2D.GetLength(0) == array2D.GetLength(1))
+{
+    // int [,] newArray = MatrixFlipOver(array2D);
+    // PrintMatrix(newArray);
+    MatrixFlipOver2(array2D);
+    PrintMatrix(array2D);
+}
+else Console.WriteLine("Ваша матрица не квадратная");
